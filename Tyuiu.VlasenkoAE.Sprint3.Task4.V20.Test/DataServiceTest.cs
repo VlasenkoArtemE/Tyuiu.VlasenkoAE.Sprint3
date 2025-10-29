a446299840af8d1e3eca3d141d293a47a67733d8
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-10-29 16:42:04 +0500

    Task4

---
+++ Tyuiu.VlasenkoAE.Sprint3.Task4.V20.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.VlasenkoAE.Sprint3.Task4.V20.Lib;
+
+namespace Tyuiu.VlasenkoAE.Sprint3.Task4.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 4.676;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoAE.Sprint3.Task4.V20/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.VlasenkoAE.Sprint3.Task4.V20.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Оператор цикла for                                                *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #20                                                             *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+Console.WriteLine("* функции y=x/(cos(x) - x)+2,5. При х = 0 пропустить значение.            *");
+Console.WriteLine("* Полученные значения перемножать.                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Произведение ряда = " + ds.Calculate(startValue, stopValue));
+
+Console.ReadKey();
