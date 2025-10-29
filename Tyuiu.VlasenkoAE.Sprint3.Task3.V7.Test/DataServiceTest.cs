d537f4a30a4d2350890b7edf07183675e14b72de
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-10-29 15:31:34 +0500

    Task3

---
+++ Tyuiu.VlasenkoAE.Sprint3.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.VlasenkoAE.Sprint3.Task3.V7.Lib;
+
+namespace Tyuiu.VlasenkoAE.Sprint3.Task3.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceCharOnNum()
+        {
+            DataService ds = new DataService();
+
+            string str = "gft hggt ntg";
+            char replaceable = 'g';
+            char replacement = '4';
+
+            string res = ds.ReplaceCharOnNum(str, replaceable, replacement);
+
+            string wait = "4ft h44t nt4";
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoAE.Sprint3.Task3.V7/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.VlasenkoAE.Sprint3.Task3.V7.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Оператор цикла for                                                *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #7                                                              *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Используя цикл foreach заменить буквы g на цифру 4                      *");
+Console.WriteLine("* в строке: gft hggt ntg.                                                 *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string value = "gft hggt ntg";
+char replaceable = 'g';
+char replacement = '4';
+
+Console.WriteLine("Исходная строка: " + value);
+Console.WriteLine("Искомый символ: " + replaceable);
+Console.WriteLine("Cимвол замены: " + replacement);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Итоговая строка = " + ds.ReplaceCharOnNum(value, replaceable, replacement));
+
+Console.ReadKey();
