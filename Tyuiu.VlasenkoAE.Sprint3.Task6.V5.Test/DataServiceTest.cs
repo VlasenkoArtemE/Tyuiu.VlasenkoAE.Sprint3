1eaa8bc09af45e2956e49d4407348d6043581ca9
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-10-31 00:42:14 +0500

    Task6

---
+++ Tyuiu.VlasenkoAE.Sprint3.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.VlasenkoAE.Sprint3.Task6.V5.Lib;
+
+namespace Tyuiu.VlasenkoAE.Sprint3.Task6.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int x = 5;
+            int startValue = 15;
+            int stopValue = 22;
+
+            double res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            double wait = 242;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoAE.Sprint3.Task6.V5/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.VlasenkoAE.Sprint3.Task6.V5.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #5                                                              *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая ищет среди целых чисел, принадлежащих       *");
+Console.WriteLine("* числовому отрезку [15, 22] сумму всех делителей.                        *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = 15;
+int stopValue = 22;
+
+Console.WriteLine("Начало отрезка = " + startValue);
+Console.WriteLine("Конец отрезка = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+Console.ReadKey();
+
